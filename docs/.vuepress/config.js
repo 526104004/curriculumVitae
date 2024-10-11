97150d79ee9872169c1c6ffed4ef20732b462e4a
OPPLE\opwbkf07 <Tian526104.>
2024-10-11 10:13:21 +0800

    1

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -8,7 +8,7 @@ export default defineUserConfig({
 
   title: 'VuePress',
   description: 'My first VuePress Site',
-  base: './',
+  base: '/curriculumVitae/',
 
   theme: defaultTheme({
     logo: 'https://vuejs.press/images/hero.png',
