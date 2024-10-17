2d30587a3d3c8da0766bbb1a8b3ce58e60161218
柳阿久 <526104004@qq.com>
2024-10-17 13:57:55 +0800

    1

--- docs/.vuepress/.temp/markdown-tab/config.js
+++ docs/.vuepress/.temp/markdown-tab/config.js
@@ -1,6 +1,6 @@
-import { CodeTabs } from "C:/Users/opwbkf07/Desktop/vuepress-starter/node_modules/.pnpm/@vuepress+plugin-markdown-tab@2.0.0-rc.54_markdown-it@14.1.0_vuepress@2.0.0-rc.18_@vuepress+b_syttwwmthu2ge4n6jxbvtznuu4/node_modules/@vuepress/plugin-markdown-tab/lib/client/components/CodeTabs.js";
-import { Tabs } from "C:/Users/opwbkf07/Desktop/vuepress-starter/node_modules/.pnpm/@vuepress+plugin-markdown-tab@2.0.0-rc.54_markdown-it@14.1.0_vuepress@2.0.0-rc.18_@vuepress+b_syttwwmthu2ge4n6jxbvtznuu4/node_modules/@vuepress/plugin-markdown-tab/lib/client/components/Tabs.js";
-import "C:/Users/opwbkf07/Desktop/vuepress-starter/node_modules/.pnpm/@vuepress+plugin-markdown-tab@2.0.0-rc.54_markdown-it@14.1.0_vuepress@2.0.0-rc.18_@vuepress+b_syttwwmthu2ge4n6jxbvtznuu4/node_modules/@vuepress/plugin-markdown-tab/lib/client/styles/vars.css";
+import { CodeTabs } from "C:/Users/opwbkf07/Desktop/curriculumVitae/node_modules/.pnpm/@vuepress+plugin-markdown-tab@2.0.0-rc.54_markdown-it@14.1.0_vuepress@2.0.0-rc.18_@vuepress+b_syttwwmthu2ge4n6jxbvtznuu4/node_modules/@vuepress/plugin-markdown-tab/lib/client/components/CodeTabs.js";
+import { Tabs } from "C:/Users/opwbkf07/Desktop/curriculumVitae/node_modules/.pnpm/@vuepress+plugin-markdown-tab@2.0.0-rc.54_markdown-it@14.1.0_vuepress@2.0.0-rc.18_@vuepress+b_syttwwmthu2ge4n6jxbvtznuu4/node_modules/@vuepress/plugin-markdown-tab/lib/client/components/Tabs.js";
+import "C:/Users/opwbkf07/Desktop/curriculumVitae/node_modules/.pnpm/@vuepress+plugin-markdown-tab@2.0.0-rc.54_markdown-it@14.1.0_vuepress@2.0.0-rc.18_@vuepress+b_syttwwmthu2ge4n6jxbvtznuu4/node_modules/@vuepress/plugin-markdown-tab/lib/client/styles/vars.css";
 
 export default {
   enhance: ({ app }) => {
